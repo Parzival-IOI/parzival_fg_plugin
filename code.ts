76e34b6f9c6907d4fcf689740469c79f4ab684de
Kimhouy HOR <bunret2015@gmail.com>
2025-04-26 06:03:01 +0700

    styling

--- code.ts
+++ code.ts
@@ -3,49 +3,27 @@
 // You can access browser APIs in the <script> tag inside "ui.html" which has a
 // full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
 
-let css = '';
-// const fs = require('node:fs');
-// fs.readFile('./ui.css', 'utf8', (err: any, data: any) => {
-//   css = "<style>" + data + "</style>";
-// });
 
 // Runs this code if the plugin is run in Figma
 if (figma.editorType === 'figma') {
-  // This plugin will open a window to prompt the user to enter a number, and
-  // it will then create that many rectangles on the screen.
 
   // This shows the HTML page in "ui.html".
-  figma.showUI(__html__, { themeColors: true});
+  figma.showUI(__html__, { themeColors: true, width: 500, height: 300,  title: "Parzival's Plugin" });
 
   // Calls to "parent.postMessage" from within the HTML page will trigger this
   // callback. The callback will be passed the "pluginMessage" property of the
   // posted message.
-  figma.ui.onmessage =  (msg: {type: string, count: number}) => {
-    // One way of distinguishing between different types of messages sent from
-    // your HTML page is to use an object with a "type" property like this.
-    if (msg.type === 'create-shapes') {
-      // This plugin creates rectangles on the screen.
-      const numberOfRectangles = msg.count;
+  figma.ui.onmessage =  (msg: {type: string, description: string}) => {
 
-      const nodes: SceneNode[] = [];
-      for (let i = 0; i < numberOfRectangles; i++) {
-        const rect = figma.createRectangle();
-        rect.x = i * 150;
-        rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-        figma.currentPage.appendChild(rect);
-        nodes.push(rect);
-      }
-      figma.currentPage.selection = nodes;
-      figma.viewport.scrollAndZoomIntoView(nodes);
+    if (msg.type === 'execute') {
+      
     }
 
-    // Make sure to close the plugin when you're done. Otherwise the plugin will
-    // keep running, which shows the cancel button at the bottom of the screen.
     figma.closePlugin();
   };
 }
 
-// Runs this code if the plugin is run in FigJam
+// FigJam
 if (figma.editorType === 'figjam') {
   // This plugin will open a window to prompt the user to enter a number, and
   // it will then create that many shapes and connectors on the screen.
