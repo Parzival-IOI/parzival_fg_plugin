2e97b12b6df7507f7e30f724d7f6057dab363dc7
Kimhouy HOR <bunret2015@gmail.com>
2025-04-29 22:07:25 +0700

    screenshot page

--- code.ts
+++ code.ts
@@ -1,78 +1,72 @@
-// This file holds the main code for plugins. Code in this file has access to
-// the *figma document* via the figma global object.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
-
-
-// Runs this code if the plugin is run in Figma
 if (figma.editorType === 'figma') {
 
-  // This shows the HTML page in "ui.html".
   figma.showUI(__html__, { themeColors: true, width: 500, height: 300,  title: "Parzival's Plugin" });
 
-  // Calls to "parent.postMessage" from within the HTML page will trigger this
-  // callback. The callback will be passed the "pluginMessage" property of the
-  // posted message.
-  figma.ui.onmessage =  (msg: {type: string, description: string}) => {
+  figma.ui.onmessage =  async (msg: {type: string, description: string}) => {
 
     if (msg.type === 'execute') {
-      
-    }
 
-    figma.closePlugin();
+      figma.ui.postMessage("hi");
+    }
+    else if (msg.type === 'screenshot') {
+
+      const originalNodes = [...figma.currentPage.children];
+      if (originalNodes.length === 0) {
+        figma.notify("No objects on current page.");
+        return;
+      }
+
+      // Create temporary frame to export
+      const tempFrame = figma.createFrame();
+      tempFrame.name = "TempExportFrame";
+      tempFrame.clipsContent = false;
+      tempFrame.layoutMode = "NONE";
+      tempFrame.x = 10000;
+      tempFrame.y = 10000;
+
+      // Move all nodes into temp frame (temporarily)
+      for (const node of originalNodes) {
+        tempFrame.appendChild(node);
+      }
+
+      console.log(figma.currentPage.children)
+
+      // Export the frame
+      const exportBytes = await tempFrame.exportAsync({
+        format: 'PNG',
+        constraint: { type: 'SCALE', value: 2 }
+      });
+
+      console.log(figma.currentPage.children)
+
+
+      // Move nodes back to original page
+      for (const node of [...tempFrame.children]) {
+        figma.currentPage.appendChild(node);
+      }
+
+      // Remove temp frame
+      tempFrame.remove();
+
+      // Convert to base64
+      const base64 = figma.base64Encode(exportBytes);
+
+      figma.ui.postMessage(base64);
+    }
+    else {
+      figma.closePlugin();
+    }
   };
 }
 
-// FigJam
 if (figma.editorType === 'figjam') {
-  // This plugin will open a window to prompt the user to enter a number, and
-  // it will then create that many shapes and connectors on the screen.
-
-  // This shows the HTML page in "ui.html".
   figma.showUI(__html__);
 
-  // Calls to "parent.postMessage" from within the HTML page will trigger this
-  // callback. The callback will be passed the "pluginMessage" property of the
-  // posted message.
-  figma.ui.onmessage =  (msg: {type: string, count: number}) => {
-    // One way of distinguishing between different types of messages sent from
-    // your HTML page is to use an object with a "type" property like this.
+  figma.ui.onmessage = async (msg: {type: string, count: number}) => {
     if (msg.type === 'create-shapes') {
-      // This plugin creates shapes and connectors on the screen.
-      const numberOfShapes = msg.count;
-
-      const nodes: SceneNode[] = [];
-      for (let i = 0; i < numberOfShapes; i++) {
-        const shape = figma.createShapeWithText();
-        // You can set shapeType to one of: 'SQUARE' | 'ELLIPSE' | 'ROUNDED_RECTANGLE' | 'DIAMOND' | 'TRIANGLE_UP' | 'TRIANGLE_DOWN' | 'PARALLELOGRAM_RIGHT' | 'PARALLELOGRAM_LEFT'
-        shape.shapeType = 'ROUNDED_RECTANGLE';
-        shape.x = i * (shape.width + 200);
-        shape.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-        figma.currentPage.appendChild(shape);
-        nodes.push(shape);
-      }
-
-      for (let i = 0; i < numberOfShapes - 1; i++) {
-        const connector = figma.createConnector();
-        connector.strokeWeight = 8;
-
-        connector.connectorStart = {
-          endpointNodeId: nodes[i].id,
-          magnet: 'AUTO',
-        };
-
-        connector.connectorEnd = {
-          endpointNodeId: nodes[i + 1].id,
-          magnet: 'AUTO',
-        };
-      }
-
-      figma.currentPage.selection = nodes;
-      figma.viewport.scrollAndZoomIntoView(nodes);
+      
     }
 
-    // Make sure to close the plugin when you're done. Otherwise the plugin will
-    // keep running, which shows the cancel button at the bottom of the screen.
     figma.closePlugin();
   };
 }
