67b9cc5ac45914a223b23157d0800ad55e168558
Kimhouy HOR <bunret2015@gmail.com>
2025-05-07 05:34:40 +0700

    component screenshot

--- code.ts
+++ code.ts
@@ -67,14 +67,86 @@ if (figma.editorType === 'figma') {
       figma.ui.postMessage({type: 'image', image: base64});
     }
     else if (msg.type === 'test') {
-      console.log("test");
-      if (figma.currentUser) {
-        console.log("ID:", figma.currentUser.id);
-        console.log("Username:", figma.currentUser.name);
-        figma.ui.postMessage({type: 'test', test: figma.currentUser});
-      } else {
-        console.log("No user info available (possibly in anonymous mode).");
+
+      const nodes = figma.currentPage.selection;
+      if (nodes.length === 0) {
+        figma.notify("No objects selected.");
+        return;
       }
+
+      let left = Infinity;
+      let top = Infinity;
+      let right = -Infinity;
+      let bottom = -Infinity;
+
+      for (const node of nodes) {
+        console.log(node.name)
+        const x = node.absoluteTransform[0][2];
+        const y = node.absoluteTransform[1][2];
+        left = Math.min(left, x);
+        top = Math.min(top, y);
+        right = Math.max(right, x + node.width);
+        bottom = Math.max(bottom, y + node.height);
+      }
+
+      const originalNodes = [...figma.currentPage.children];
+
+      const tempFrame = figma.createFrame();
+      tempFrame.name = "TempExportFrame";
+      tempFrame.clipsContent = false;
+      tempFrame.layoutMode = "NONE";
+      tempFrame.x = 10000;
+      tempFrame.y = 10000;
+      //tempFrame.resize(right - left, bottom - top);
+      tempFrame.fills = [];
+
+
+      console.log(`left ${left}, right ${right}, top ${top}, bottom ${bottom}`);
+
+
+      for (const node of originalNodes) {
+
+        const clone_top = node.absoluteTransform[1][2];
+        const clone_left = node.absoluteTransform[0][2];
+        const clone_right = node.absoluteTransform[0][2] + node.width;
+        const clone_bottom = node.absoluteTransform[1][2] + node.height;
+
+        if (
+          //( left <= clone_right && right >= clone_left) &&
+          //( top <= clone_bottom && bottom >= clone_top)
+          left <= clone_left && right >= clone_right &&
+          top <= clone_top && bottom >= clone_bottom
+        ) {
+
+          console.log(`name ${node.name} clone_left ${clone_left}, clone_right ${clone_right}, clone_top ${clone_top}, clone_bottom ${clone_bottom}`);
+          
+          if ("clone" in node) {
+            const clone = node.clone();
+
+            const originalX = node.absoluteTransform[0][2];
+            const originalY = node.absoluteTransform[1][2];
+
+            // Position relative to tempFrame
+            clone.x = originalX - left;
+            clone.y = originalY - top;
+            tempFrame.appendChild(clone);
+          }
+        }
+      }
+      
+      const exportBytes = await tempFrame.exportAsync({
+        format: 'PNG',
+        constraint: { type: 'SCALE', value: 2 }
+      });
+
+      tempFrame.remove();
+
+      
+
+
+      const base64 = figma.base64Encode(exportBytes);
+
+      figma.ui.postMessage({type: 'image', image: base64});
       
     }
     else {
